8f96c6ee0d79ebc0d4e1402b88b12360b99de037
Michael Ingrisch <michael.ingrisch@med.lmu.de>
2014-10-29 07:50:08 +0100

    more tests for the generic model

--- tests/test_genericmodel.py
+++ tests/test_genericmodel.py
@@ -29,3 +29,17 @@ def test_genericModel_set_and_get_time(model):
     time = sp.linspace(0, 20, 50)
     model.set_time(time)
     np.testing.assert_array_equal(model.get_time(), time)
+    
+def test_genericModel_set_and_get_aif(model):
+    aif = np.random.randn(50)
+    model.set_aif(aif)
+    np.testing.assert_array_equal(model.get_aif(), aif)
+    
+def test_genericModel_set_and_get_curve(model):
+    curve = np.random.rand(50)
+    model.set_curve(curve)
+    np.testing.assert_array_equal(model.get_curve(), curve)
+    
+def test_genericModel_readableParameters_contain_all_keys(model):
+    assert all(k in model.readable_parameters for k in  ("F", "v", "MTT"))
+
