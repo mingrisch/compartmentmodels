d2caf8a39b8cd80d85c4d7ea03cf76b4827f1b66
Michael Ingrisch <michael.ingrisch@med.lmu.de>
2014-10-29 16:37:05 +0100

    testcase for convolution added (issue #5)

--- tests/create_testcase_convolution.py
+++ tests/create_testcase_convolution.py
@@ -2,7 +2,7 @@
 import numpy as np
 from scipy import optimize
 
-from compartmentmodels.GenericModel import GenericModel
+from compartmentmodels.GenericModelOriginal import GenericModel
         
 if __name__ == '__main__':
     # create  a boxcar function for the aif and calculate some convolutions
@@ -25,17 +25,19 @@ if __name__ == '__main__':
     lambdalist =[3.2, 1.8, 0.2, 0.0000110]
 
     # set up an array for the outfile
-    fileheader=['aif']
+    fileheader=['time', 'aif']
     for el in lambdalist:
         fileheader.append(str(el))
                 
     outarray=np.zeros((len(fileheader),npt))
+    outarray[0,:]=time
+    outarray[1,:]=aif
 
     fileheader=','.join(fileheader)
     
     for i, lam in enumerate(lambdalist):
         curve=gp.convolution_w_exp(lam)
-        outarray[i+1,:]=curve
+        outarray[i+2,:]=curve
         print "Mean of convolution with {}: {}".format(lam, curve.mean())
 
     saved=np.savetxt('convolutions.csv', outarray.transpose(), header=fileheader)
